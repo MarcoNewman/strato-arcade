61610a791192d268a814bb92ea4cb5e0b1998812
Marco <marcoanewman@gmail.com>
2021-07-21 16:03:09 -0600

    refined multi pixy data piping

--- test_pixys.py
+++ test_pixys.py
@@ -1,7 +1,7 @@
 """
 BIRST Team 1
 Strato-Arcade
-Pixy Test Script
+Multi-Pixy Test Script
 POC: Marco Newman
 """
 from datetime import datetime
@@ -9,15 +9,15 @@ import pixy2.build.python_demos.pixy as pixy
 from ctypes import *
 from pixy2.build.python_demos.pixy import *
 
-print("Pixy2 Python Test -- Get/Save Blocks")
+print("Multiple Pixy2 Python Test -- Get/Save Blocks")
+t='{:%Y%m%d-%H%M%S}'.format(datetime.now())
 
-# Initialize Data Stores
-# |time|block_id|block_x|block_y|block_width|block_height| - Pixy_Logs
-with open(f"/home/pi/BIRST/logs/Pixy1_Blocks.csv", "w") as log:
-  log.write("time, pixy_id, signature, block_x, block_y, block_width, block_height\n")
-with open(f"/home/pi/BIRST/logs/Pixy2_Blocks.csv", "w") as log:
-  log.write("time, pixy_id, signature, block_x, block_y, block_width, block_height\n")
+# Initialize log data store
+# |time|pixy_id|signature_id|block_x|block_y|block_width|block_height| - Pixy_Logs
+with open(f"/home/pi/BIRST/logs/Pixy2s_{t}.csv", "w") as log:
+  log.write("time, pixy_id, signature_id, block_x, block_y, block_width, block_height\n")
 
+# Initialize Pixy2s
 a = pixy.init (1)
 print(f"Pixy1 Initialized: {a}")
 pixy.change_prog ("color_connected_components", 1)
@@ -25,6 +25,7 @@ b = pixy.init (2)
 print(f"Pixy2 Initialized: {b}")
 pixy.change_prog ("color_connected_components", 2)
 
+# Define block data structure for tracks
 class Blocks (Structure):
   _fields_ = [ ("m_signature", c_uint),
     ("m_x", c_uint),
@@ -34,12 +35,13 @@ class Blocks (Structure):
     ("m_angle", c_uint),
     ("m_index", c_uint),
     ("m_age", c_uint) ]
-
 pixy1_blocks = BlockArray(100)
 pixy2_blocks = BlockArray(100)
-frame = 0
+pixy1_frame = 0
+pixy2_frame = 0
 
 while 1:
+  # Check both pixys for track blocks
   for pixy_id in [1,2]:
     if pixy_id == 1:
       count = pixy.ccc_get_blocks (100, pixy1_blocks, pixy_id)
@@ -47,23 +49,31 @@ while 1:
       count = pixy.ccc_get_blocks (100, pixy2_blocks, pixy_id)
 
     if count > 0:
+      # Blocks detected -> update frame count and timestamp
       time_now = datetime.now()
-      print(f'PixyID: {pixy_id} | Frame: {frame} | {time_now}')
-      frame = frame + 1
+      if pixy_id == 1:
+        print(f'PixyID: {pixy_id} | Frame: {pixy1_frame} | {time_now}')
+        pixy1_frame = pixy1_frame + 1
+      else:
+        print(f'PixyID: {pixy_id} | Frame: {pixy2_frame} | {time_now}')
+        pixy2_frame = pixy2_frame + 1
+
+      # Read block position data and size
       for index in range (0, count):
         if pixy_id == 1:
-          sig = pixy1_blocks[index].m_signature
+          signature_id = pixy1_blocks[index].m_signature
           block_x = pixy1_blocks[index].m_x
           block_y = pixy1_blocks[index].m_y
           block_width = pixy1_blocks[index].m_width
           block_height = pixy1_blocks[index].m_height
         else:
-          sig = pixy2_blocks[index].m_signature
+          signature_id = pixy2_blocks[index].m_signature
           block_x = pixy2_blocks[index].m_x
           block_y = pixy2_blocks[index].m_y
           block_width = pixy2_blocks[index].m_width
           block_height = pixy2_blocks[index].m_height
 
-        print(f'[BLOCK: SIG={sig:d} X={block_x:3d} Y={block_y:3d} WIDTH={block_width:3d} HEIGHT={block_height:3d}]')
-        with open(f"/home/pi/BIRST/logs/Pixy{pixy_id}_Blocks.csv", "a") as log:
-          log.write(f"{time_now}, {sig:d}, {block_x:3d}, {block_y:3d}, {block_width:3d}, {block_height:3d}\n")
+        # Report block data
+        print(f'[BLOCK: SIG={signature_id:d} X={block_x:3d} Y={block_y:3d} WIDTH={block_width:3d} HEIGHT={block_height:3d}]')
+        with open(f"/home/pi/BIRST/logs/Pixy2s_{t}.csv", "a") as log:
+          log.write(f"{time_now}, {pixy_id:d}, {signature_id:d}, {block_x:3d}, {block_y:3d}, {block_width:3d}, {block_height:3d}\n")
